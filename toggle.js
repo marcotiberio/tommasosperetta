7f7f1263d1fcfc701ef6a10fa1a71b06c974495c
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2019-04-17 15:00:25 +0200

    updated toggle index.html, style.css + created toggle.js

---
+++ toggle.js
@@ -0,0 +1,18 @@
+// SIDEBAR
+$("#title").click(function () {
+  $("#sidebar").toggle();
+});
+$("#sidebar-close").click(function () {
+  $("#sidebar").hide();
+});
+
+
+$(".table-project_title40").click(function(){
+  $("#table-project_details40").toggle();
+});
+$(".table-project_title39").click(function(){
+  $("#table-project_details39").toggle();
+});
+$(".table-project_title37").click(function(){
+  $("#table-project_details37").toggle();
+});
