976c5eb70e94a4433d4bc8a294b293a3ffa7a317
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2019-04-17 16:53:49 +0200

    updated all toggles

--- toggle.js
+++ toggle.js
@@ -7,12 +7,115 @@ $("#sidebar-close").click(function () {
 });
 
 
+// TABLE ROWS
 $(".table-project_title40").click(function(){
   $("#table-project_details40").toggle();
 });
 $(".table-project_title39").click(function(){
   $("#table-project_details39").toggle();
 });
-$(".table-project_title37").click(function(){
-  $("#table-project_details37").toggle();
+$(".table-project_title38").click(function(){
+  $("#table-project_details38").toggle();
+});
+$(".table-project_title36").click(function(){
+  $("#table-project_details36").toggle();
+});
+$(".table-project_title35").click(function(){
+  $("#table-project_details35").toggle();
+});
+$(".table-project_title34").click(function(){
+  $("#table-project_details34").toggle();
+});
+$(".table-project_title33").click(function(){
+  $("#table-project_details33").toggle();
+});
+$(".table-project_title32").click(function(){
+  $("#table-project_details32").toggle();
+});
+$(".table-project_title31").click(function(){
+  $("#table-project_details31").toggle();
+});
+$(".table-project_title30").click(function(){
+  $("#table-project_details30").toggle();
+});
+$(".table-project_title29").click(function(){
+  $("#table-project_details29").toggle();
+});
+$(".table-project_title28").click(function(){
+  $("#table-project_details28").toggle();
+});
+$(".table-project_title27").click(function(){
+  $("#table-project_details27").toggle();
+});
+$(".table-project_title26").click(function(){
+  $("#table-project_details26").toggle();
+});
+$(".table-project_title25").click(function(){
+  $("#table-project_details25").toggle();
+});
+$(".table-project_title24").click(function(){
+  $("#table-project_details24").toggle();
+});
+$(".table-project_title23").click(function(){
+  $("#table-project_details23").toggle();
+});
+$(".table-project_title22").click(function(){
+  $("#table-project_details22").toggle();
+});
+$(".table-project_title21").click(function(){
+  $("#table-project_details21").toggle();
+});
+$(".table-project_title20").click(function(){
+  $("#table-project_details20").toggle();
+});
+$(".table-project_title18").click(function(){
+  $("#table-project_details18").toggle();
+});
+$(".table-project_title17").click(function(){
+  $("#table-project_details17").toggle();
+});
+$(".table-project_title16").click(function(){
+  $("#table-project_details16").toggle();
+});
+$(".table-project_title15").click(function(){
+  $("#table-project_details15").toggle();
+});
+$(".table-project_title14").click(function(){
+  $("#table-project_details14").toggle();
+});
+$(".table-project_title13").click(function(){
+  $("#table-project_details13").toggle();
+});
+$(".table-project_title12").click(function(){
+  $("#table-project_details12").toggle();
+});
+$(".table-project_title11").click(function(){
+  $("#table-project_details11").toggle();
+});
+$(".table-project_title10").click(function(){
+  $("#table-project_details10").toggle();
+});
+$(".table-project_title9").click(function(){
+  $("#table-project_details9").toggle();
+});
+$(".table-project_title7").click(function(){
+  $("#table-project_details7").toggle();
+});
+$(".table-project_title6").click(function(){
+  $("#table-project_details6").toggle();
+});
+$(".table-project_title5").click(function(){
+  $("#table-project_details5").toggle();
+});
+$(".table-project_title4").click(function(){
+  $("#table-project_details4").toggle();
+});
+$(".table-project_title3").click(function(){
+  $("#table-project_details3").toggle();
+});
+$(".table-project_title2").click(function(){
+  $("#table-project_details2").toggle();
+});
+$(".table-project_title1").click(function(){
+  $("#table-project_details1").toggle();
 });
