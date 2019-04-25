4831815f49ae32e18bb6215047663d130a5b2f46
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2019-04-25 13:48:12 +0200

    loader

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+// LOADER
+
+jQuery(document).ready(function(){
+  /* Show the HTML page only after the js and css are completely loaded */
+  delayShow();
+});
+
+function delayShow() {
+  var secs = 1000;
+  setTimeout('jQuery("body").css("visibility","visible");', secs);
+}
+
+
 // LIGHTBOX
 // $(document).on('click', '[data-toggle="lightbox"]', function(event) {
 //     event.preventDefault();
